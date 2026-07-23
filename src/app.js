63bdc24b33ae5e1fbbe8efef9f7bfa77902cf6ac
ChanduAkula7 <chandrakanth3335@gmail.com>
2026-07-23 23:31:32 +0530

    middleware errors

--- src/app.js
+++ src/app.js
@@ -5,8 +5,32 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello from server");
+const router = express.Router();
+//this wikk register an middleware for all the routes which are registered in this router
+app.use("/api", router);
+
+router.use((req, res, next) => {
+  console.log("middleware is running---Pre-chcek has done");
+  next();
+});
+
+//want to add global error handling middleware
+
+router.get("/user", (req, res) => {
+  throw new Error("KABDCBASDHY");
+  res
+    .status(200)
+    .json({ message: "user is responding-router", HELLO: "hello" });
+});
+
+//wild card of error handling middleware
+//always keep on end of the excection
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res
+      .status(500)
+      .json({ message: "something went wrong", error: err.message });
+  }
 });
 app.listen(3000, () => {
   console.log("server is running on port 3000");
